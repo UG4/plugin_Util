ad4d10fffd3232d76e2df3cdba8fa18214521312
irorita <myrto.pap@stud.uni-frankfurt.de>
2025-10-05 16:40:15 +0200

    fix bug on NewtonSolver.createLineSearch accepting nil or none

--- solver_util.h
+++ solver_util.h
@@ -676,42 +676,67 @@ namespace ug
                 SmartPtr<StdConvCheck<typename TAlgebra::vector_type>> convCheck;
                 convCheck = CreateConvCheck<TAlgebra>(convCheckDesc);
                 newtonSolver->set_convergence_check(convCheck);
-
-                // line search json default
-                nlohmann::json lineSearchDesc;
-                std::string lineSearchType;
                 
+                // line search
+                nlohmann::json              lineSearchDesc = nullptr;
+                std::optional<std::string>  lineSearchType;
+
+                // defaults JSON (can be string, object or null)
                 if (json_default_nonlinearSolver["newton"]["lineSearch"].is_string()){
-                    lineSearchType = json_default_nonlinearSolver["newton"]["lineSearch"];
+                    lineSearchType = json_default_nonlinearSolver["newton"]["lineSearch"].get<std::string>();
                 }
                 else if (json_default_nonlinearSolver["newton"]["lineSearch"].is_object()){
                     lineSearchDesc = json_default_nonlinearSolver["newton"]["lineSearch"];
                 }
-                else
-                    UG_LOG(">> LineSearchDesc in json_defaults is null \n")
+                else {
+                    UG_LOG(">> LineSearchDesc in json_defaults is null \n");
+                }
                 
-                // get descriptor for line search
-                if (solverDesc.contains("lineSearch") && solverDesc["lineSearch"].is_string()){
-                    // UG_LOG(">> LineSearchDesc is a string from solverDesc\n");
-                    lineSearchType = solverDesc["lineSearch"];
-                    lineSearchDesc["type"] = lineSearchType;
+                // get descriptor for line search (user override)
+                if (solverDesc.contains("lineSearch") && !solverDesc["lineSearch"].is_null()){
+                    if (solverDesc["lineSearch"].is_string()){
+                        // UG_LOG(">> LineSearchDesc is a string from solverDesc\n");
+                        lineSearchType = solverDesc["lineSearch"].get<std::string>();
+                    }
+                    else if (solverDesc["lineSearch"].is_object()){
+                        // UG_LOG(">> LineSearchDesc is an object from solverDesc\n");
+                        lineSearchDesc = solverDesc["lineSearch"];
+                    }
+                    else {
+                        UG_LOG(">> solverDesc.lineSearch has unsupported type -> ignored\n");
+                    }
+                }   
+                else {
+                    UG_LOG(">> LineSearchDesc for NewtonSolver, default in desc is null or missing\n");
                 }
-                else if (solverDesc.contains("lineSearch") && solverDesc["lineSearch"].is_object()){
-                    // UG_LOG(">> LineSearchDesc is an object from solverDesc\n");
-                    lineSearchDesc = solverDesc["lineSearch"];
-                }
-                else{
-                    UG_LOG(">> LineSearchDesc for NewtonSolver, default in desc is null \n");
-                }
-                 
-                // if line_Search not null
-                if (!lineSearchDesc.empty() && lineSearchDesc != "none"){
+                
+                // Decide if valid string or object
+                bool have_valid_string = lineSearchType.has_value() && !lineSearchType->empty() && *lineSearchType != "none";
+                bool have_valid_object = lineSearchDesc.is_object() &&
+                                         lineSearchDesc.contains("type") &&
+                                         lineSearchDesc["type"].is_string() &&
+                                         lineSearchDesc["type"].get<std::string>() != "none";
+                
+                // Set line Search on newton Solver if valid string or object
+                if (have_valid_string || have_valid_object){
+                    if (have_valid_string && !lineSearchDesc.is_object()){
+                        lineSearchDesc = nlohmann::json::object();
+                        lineSearchDesc["type"] = *lineSearchType;
+                    }
+                    else if (have_valid_string && lineSearchDesc.is_object() && !lineSearchDesc.contains("type")){
+                        lineSearchDesc["type"] = *lineSearchType;
+                    }
+
+                    std::cout << "[DEBUG] input to CreateLineSearch:\n" << lineSearchDesc.dump(4) << std::endl;
+
                     SmartPtr<StandardLineSearch<typename TAlgebra::vector_type>> lineSearch;
-                    std::cout << "[DEBUG] input to CreateLineSearch:\n" << solverDesc.dump(4) << std::endl;
                     lineSearch = CreateLineSearch<TAlgebra>(lineSearchDesc);
                     newtonSolver->set_line_search(lineSearch);
                 }
-                
+                else {
+                    UG_LOG(">> No line search configured (nil or 'none') - skipping CreateLineSearch.\n");
+                }
+
                 // set reassemble_J_freq if exists
                 if (solverDesc.contains("reassemble_J_freq") && solverDesc["reassemble_J_freq"].is_number()){
                     newtonSolver->set_reassemble_J_freq(solverDesc["reassemble_J_freq"]);
